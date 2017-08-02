4646b18981b058312d99e576a8422287ee2700ef
John Ashton <John_ashton@hotmail.com>
2017-08-02 16:41:29 +0100

    Add new post and show on index

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
-
+Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
+Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 # Initialize the Rails application.
 Rails.application.initialize!
